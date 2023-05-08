5d79fbe5f7c41b161bc54fd71d51faec64b6c1c0
rednoj5 <antoszewskijedrzej@gmail.com>
2023-05-08 21:33:15 +0200

    Implemented initial UI and styling implemented grid sizer and connected to JS

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-const board = document.querySelector('.board')
+window.onload = () => createDivs(16);
+
+const board = document.querySelector('.board');
+const slider = document.querySelector('#slider');
+const sliderOutput = document.querySelector('#gridSizeOutput')
+
+//functions
 
 function createDivs(x) {
     for (i = 0; i < x; i++) {
@@ -8,9 +14,26 @@ function createDivs(x) {
         for (y = 0; y < x; y++) {
             let cell = document.createElement('div');
             cell.setAttribute('class', 'cell');
+            cell.addEventListener('mouseover', changeColor);
             row.appendChild(cell);
         }
     };
 };
 
-createDivs(16);
+function clearBoard() {
+    board.innerHTML = '';
+};
+
+function changeColor(e) {
+    e.target.setAttribute('id', 'black');
+};
+
+function updateSlider() {
+    let size = slider.value;
+    sliderOutput.textContent = `${size} x ${size}`;
+}
+
+slider.addEventListener('change', updateSlider);
+slider.addEventListener('mousemove', updateSlider);
+
+console.log(slider.value)
