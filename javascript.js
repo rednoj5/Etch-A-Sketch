ba6a7de56fc676c3e9019d67b9bd029b6de1a9c5
rednoj5 <antoszewskijedrzej@gmail.com>
2023-05-09 21:52:32 +0200

    Implemented rgb mode, shading mode, erase mode Optimised functions Color pick mode only left to be added

--- javascript.js
+++ javascript.js
@@ -2,7 +2,12 @@ window.onload = () => createDivs(16);
 
 const board = document.querySelector('.board');
 const slider = document.querySelector('#slider');
-const sliderOutput = document.querySelector('#gridSizeOutput')
+const sliderOutput = document.querySelector('#gridSizeOutput');
+const body = document.querySelector('body');
+const clearButton = document.querySelector('#clearGrid');
+const rgbButton = document.querySelector('#rgbButton');
+const shadingButton = document.querySelector('#shadingButton');
+const eraseButton = document.querySelector('#eraseButton');
 
 //functions
 
@@ -25,15 +30,73 @@ function clearBoard() {
 };
 
 function changeColor(e) {
-    e.target.setAttribute('id', 'black');
+    if (mouseDown === true) {
+        if (rgbButton.checked) {
+            e.target.style.cssText = `background-color: rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()});`;
+        } else if (shadingButton.checked) {
+            let currentOpacity = parseFloat(getComputedStyle(e.target).opacity);
+            let currentBackground = getComputedStyle(e.target).backgroundColor;
+            console.log(currentBackground);
+            if (currentOpacity > 0 && currentOpacity != 1) {
+                e.target.style.cssText = `background-color: black; opacity: ${currentOpacity + 0.1}`
+            } else if (currentOpacity == 1 && currentBackground == 'rgb(0, 0, 0)') {
+                return
+            } else {
+                e.target.style.cssText = 'background-color: black; opacity: 0.1;';
+            };
+        } else if (eraseButton.checked) {
+            e.target.style.cssText = 'background-color: ;'
+        } else {
+            e.target.style.cssText = 'background-color: black;';
+    }};
 };
 
-function updateSlider() {
+function updateSliderText() {
     let size = slider.value;
     sliderOutput.textContent = `${size} x ${size}`;
-}
+};
 
-slider.addEventListener('change', updateSlider);
-slider.addEventListener('mousemove', updateSlider);
+function updateGrid() {
+    let size = slider.value;
+    sliderOutput.textContent = `${size} x ${size}`;
+    clearBoard();
+    createDivs(size);
+};
 
-console.log(slider.value)
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
+};
+
+//DOM manipulation (using different methods to learn more :))
+
+slider.addEventListener('change', updateGrid);
+slider.addEventListener('mousemove', updateSliderText);
+
+let mouseDown = false;
+
+body.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+body.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
+
+clearButton.onclick = () => {
+    clearBoard();
+    createDivs(slider.value);
+};
+
+shadingButton.onchange = () => {
+    eraseButton.checked = false;
+    rgbButton.checked = false;
+};
+
+rgbButton.onchange = () => {
+    shadingButton.checked = false;
+    eraseButton.checked = false;
+};
+
+eraseButton.onchange = () => {
+    shadingButton.checked = false;
+    rgbButton.checked = false;
+};
